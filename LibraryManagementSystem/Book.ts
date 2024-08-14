98fc81ff4f2b186d2a0d3928d2bab670b398900a
Ram.Rv <ram.rv@flender.com>
2024-08-14 12:22:36 +0530

    Typescript (LMS): OOPS BASICS

---
+++ LibraryManagementSystem/Book.ts
@@ -0,0 +1,22 @@
+class Book{
+    title: string;
+    author: string;
+    isbn: string;
+    isAvailable: boolean;
+
+    constructor(title: string, author: string, isbn: string, isAvailable: boolean) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.isAvailable = isAvailable;
+    }
+
+    borrowBook = (): boolean => {
+        return this.isAvailable = false;
+    }
+
+    returnBook = () : boolean => {
+        return this.isAvailable = true;
+}
+
+}
---
+++ LibraryManagementSystem/Person.ts
@@ -0,0 +1,15 @@
+class Person {
+    id: number
+    name: string
+    email: string
+
+    constructor(id: number, name: string, email: string) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    displayDetails = () : void => {
+        console.log(`ID: ${this.id} ,Name: ${this.name}, Email: ${this.email}`)
+    }
+}
